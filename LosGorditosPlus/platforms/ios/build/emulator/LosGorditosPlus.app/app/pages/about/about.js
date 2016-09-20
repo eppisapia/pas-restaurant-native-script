b24e49d15e93feb295e5fa811f48ee655c74f072
eppisapia <eduardo.pisapia@synergy-gb.com>
2016-09-20 15:03:15 -0430

    Aplicacion LosGorditosPlus v1

--- LosGorditosPlus/platforms/ios/build/emulator/LosGorditosPlus.app/app/pages/about/about.js
+++ LosGorditosPlus/platforms/ios/build/emulator/LosGorditosPlus.app/app/pages/about/about.js
@@ -2,44 +2,40 @@
 var BasePage_1 = require("../../shared/BasePage");
 var observable_1 = require("data/observable");
 var platform_1 = require("platform");
+var frame_1 = require("ui/frame");
 var vm = new observable_1.Observable({
     header_resena: 0,
     header_location: 0,
     ubicaciones: ([
-        { zona: "Altamira",
-            mensaje: "Abierto hasta las 9:00pm",
-            data: ([{ latitude: 10.498086655450642,
-                    longitude: -66.85348734185897,
-                    title: "Caracas",
-                    snip: "Venezuela",
-                    dir: "Synergy-Gb, Piso 3 Centro Gerencial Mohedano,Avenida Los Chaguaramos, Caracas, Distrito Capital",
-                    gerente: "Alexander Ramirez",
-                    fijo: "0212-1234567",
-                    movil: "0414-1112233" }]),
+        { mensaje: "Abierto hasta las 9:00pm",
+            latitude: 10.498086655450642,
+            longitude: -66.85348734185897,
+            title: "Altamira",
+            snip: "Venezuela",
+            dir: "Synergy-Gb, Piso 3 Centro Gerencial Mohedano,Avenida Los Chaguaramos, Caracas, Distrito Capital",
+            gerente: "Alexander Ramirez",
+            fijo: "0212-1234567",
+            movil: "0414-1112233",
         },
-        { zona: "Carrizal",
-            mensaje: "Abierto hasta las 9:00pm",
-            data: ([
-                { latitude: 10.347091,
-                    longitude: -66.992912,
-                    title: "Los Teques",
-                    snip: "Carrizal",
-                    dir: 'C.C La Cascada, Nivel Feria, Los Teques, Miranda',
-                    gerente: "Alexander Ramirez",
-                    fijo: "0212-1234567",
-                    movil: "0414-1112233" }]),
+        { mensaje: "Abierto hasta las 9:00pm",
+            latitude: 10.347091,
+            longitude: -66.992912,
+            title: "Los Teques",
+            snip: "Carrizal",
+            dir: 'C.C La Cascada, Nivel Feria, Los Teques, Miranda',
+            gerente: "Alexander Ramirez",
+            fijo: "0212-1234567",
+            movil: "0414-1112233",
         },
-        { zona: "La Castellana",
-            mensaje: "Abierto hasta las 9:00pm",
-            data: ([
-                { latitude: 10.50,
-                    longitude: -66.86,
-                    title: "Los Jardines",
-                    snip: "Distrito Capital",
-                    dir: "Los Jardines,Avenida Los Samám, Caracas, Distrito Capital",
-                    gerente: "Ali Suarez",
-                    fijo: "0212-1234567",
-                    movil: "0414-1112233" }]),
+        { mensaje: "Abierto hasta las 9:00pm",
+            latitude: 10.50,
+            longitude: -66.86,
+            title: "Los Jardines",
+            snip: "Distrito Capital",
+            dir: "Los Jardines,Avenida Los Samám, Caracas, Distrito Capital",
+            gerente: "Ali Suarez",
+            fijo: "0212-1234567",
+            movil: "0414-1112233",
         }
     ]),
     width: platform_1.screen.mainScreen.widthDIPs,
@@ -73,8 +69,26 @@ var AboutPage = (function (_super) {
         }
     };
     AboutPage.prototype.Sede_Location = function (args) {
-        var inf = args.data;
-        console.log("Clicked on " + args.data);
+        var arg_latitude = args.view.latitude;
+        var arg_longitude = args.view.longitude;
+        var arg_dir = args.view.dir;
+        var arg_gerente = args.view.gerente;
+        var arg_fijo = args.view.fijo;
+        var arg_movil = args.view.movil;
+        var navigationOptions = {
+            moduleName: 'pages/locationdetail/locationdetail',
+            context: {
+                mark: {
+                    latitude: arg_latitude,
+                    longitude: arg_longitude,
+                    dir: arg_dir,
+                    gerente: arg_gerente,
+                    fijo: arg_fijo,
+                    movil: arg_movil
+                }
+            }
+        };
+        frame_1.topmost().navigate(navigationOptions);
     };
     return AboutPage;
 }(BasePage_1.BasePage));
